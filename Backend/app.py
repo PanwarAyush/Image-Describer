c36777eb085834b5dd9c769df459814872114d93
Ayush Panwar <ayushpanwar691@gmail.com>
2022-11-13 12:14:07 +0530

    Backend-flask

---
+++ Backend/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b9fa8861342e101e3c86743ac8a5d12d37bea2be7b0bbc10f13354fbec439364
+size 727
---
+++ Backend/caption_it.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f84835ac6e2df906383f4b4c9347301a41f6d278e7008eadcb596e4782275f75
+size 2307
